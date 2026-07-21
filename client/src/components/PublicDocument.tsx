32ea4e8dc57bcb8b3d1abbc49034e7a3b1261814
danielpehkonen <danielpehkonen21@gmail.com>
2026-07-21 16:16:47 +0300

    finished public document sharing

--- client/src/components/PublicDocument.tsx
+++ client/src/components/PublicDocument.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Alert from '@mui/material/Alert';
 import Paper from '@mui/material/Paper';
-import { Box, TextField }from '@mui/material';
+import { Box, TextField, Typography }from '@mui/material';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 interface IPublicDocument {
     title: string,
@@ -18,7 +20,7 @@ export const PublicDocument = () => {
     useEffect(() => {
         const getPublicDocument = async () => {
             try {
-                const response = await  fetch(`api/document/public/${shareToken}`);
+                const response = await  fetch(`/api/document/public/${shareToken}`);
 
                 const data = await response.json();
                 
@@ -40,7 +42,69 @@ export const PublicDocument = () => {
             getPublicDocument()
         }
     }, [shareToken])
+
+    if (loading) {
+        return (
+            <Typography>
+                Loading...
+            </Typography>
+        )
+    }
+
+    if (error || !document) {
+        return (
+            <Alert severity="error">
+                {error || "Document not found"}
+            </Alert>
+        )
+    }
+
   return (
-    <div>PublicDocument</div>
+    <Box
+        sx={{
+            width: "100%",
+            maxWidth: 1000,
+            mx: "auto",
+            px: 2,
+            py: 4,
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2
+            }}
+    >
+        <Paper
+            elevation={1}
+            sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2
+            }}
+        >
+            <TextField 
+                label="Document title"
+                value={document.title}
+                slotProps={{input: {readOnly: true}}}
+            />
+
+            <Box
+                sx={{
+                    backgroundColor: "background.paper",
+                    "& .ql-editor": {minHeight: 400}
+                }}
+            >
+                <ReactQuill
+                    theme="snow"
+                    value={document.content}
+                    readOnly
+                    modules={{toolbar: false}}
+                />
+
+            </Box>
+
+        </Paper>
+
+    </Box>
   )
 }
